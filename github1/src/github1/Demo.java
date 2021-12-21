0be597763df9ce7b3b28053d696ded2f459b64a8
Roh <Roh@Roh-PC>
2021-12-21 11:01:45 +0530

    # WARNING: head commit changed in the meantime
    by shivani

---
+++ github1/src/github1/Demo.java
@@ -0,0 +1,24 @@
+package github1;
+
+
+
+
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("my first demo");
+		System.out.println("my second demo");
+		System.out.println("my third demo");
+		
+	}
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+	public void m2()
+	{
+		System.out.println("m2 method");
+	}
+}
+	
